795d3decae0c0d8917c2f94015c33e04c5ae6f54
Carlos Daniel Bayona <carlosdanielbayo@gmail.com>
2018-12-26 19:25:17 -0500

    TaskReactiveForms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validator, Validators} from '@angular/forms';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TaskReactiveForms';
+export class AppComponent implements  OnInit {
+  reactiveForm: FormGroup;
+  projectStatus = ['Stable', 'Critical', 'Finished'];
+  forbiddenProyectNames = ['Test'];
+  onSubmit () {
+    console.log(this.reactiveForm.value);
+    this.reactiveForm.reset({
+      'status': 'Stable'
+    });
+  }
+  forbiddenNames(control: FormControl): {[s: string]: boolean} {
+    if (this.forbiddenProyectNames.indexOf(control.value) !== -1) {
+      return {'nameIsForbbiden': true};
+    }
+    return null;
+  }
+
+  forbiddenNamesAsync(control: FormControl): Promise<any> | Observable<any> {
+    const promise = new Promise<any>((resolve, reject) => {
+      setTimeout(() => {
+        if (control.value === 'Test') {
+          resolve({'nameIsForbbiden': true});
+        } else {
+          resolve(null);
+        }
+      }, 1500);
+    });
+    return promise;
+  }
+
+  ngOnInit(): void {
+    this.reactiveForm = new FormGroup({
+      'projectName': new FormControl(null, [Validators.required], this.forbiddenNamesAsync),
+      'email': new FormControl(null, [Validators.required, Validators.email]),
+      'status': new FormControl('Stable')
+      });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCardModule, MatInputModule, MatRadioModule, MatSelectModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatInputModule,
+    MatRadioModule,
+    MatCardModule,
+    MatSelectModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
